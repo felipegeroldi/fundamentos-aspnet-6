f2539b05a1e816ae172758f6694632bc8284556f
Felipe Geroldi <42786299+felipegeroldi@users.noreply.github.com>
2022-05-05 13:05:33 -0300

    ASP.NET 6 API - CRUD

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Todo.Data;
+using Todo.Models;
 
 namespace Todo.Controllers
 {
@@ -6,9 +8,61 @@ namespace Todo.Controllers
     public class HomeController : ControllerBase
     {
         [HttpGet("/")]
-        public string Get()
+        public IEnumerable<TodoModel> Get([FromServices] AppDbContext context)
         {
-            return "Hello, World!";
+            return context.Todos.ToList();
+        }
+
+        [HttpGet("/{id:int}")]
+        public TodoModel GetById(
+            [FromRoute] int id,
+            [FromServices] AppDbContext context)
+        {
+            return context.Todos.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPost("/")]
+        public TodoModel Post(
+            [FromBody] TodoModel todo,
+            [FromServices] AppDbContext context)
+        {
+            context.Todos.Add(todo);
+            context.SaveChanges();
+
+            return todo;
+        }
+
+        [HttpPut("/{id:int}")]
+        public TodoModel Put(
+            [FromRoute] int id,
+            [FromBody] TodoModel todo,
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+
+            if (model == null)
+                return todo;
+
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+
+            context.Todos.Update(model);
+            context.SaveChanges();
+
+            return model;
+        }
+
+        [HttpDelete("/{id:int}")]
+        public TodoModel Delete(
+            [FromRoute] int id,
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+
+            context.Todos.Remove(model);
+            context.SaveChanges();
+
+            return model;
         }
     }
 }
